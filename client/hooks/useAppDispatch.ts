830f6ee532690c4fff8eccf1da883b9817341a92
quanhuynh1903-cyber <quanhuynh1903@gmail.com>
2026-01-11 20:04:58 +0700

    Update useAppDispatch.ts

--- client/hooks/useAppDispatch.ts
+++ client/hooks/useAppDispatch.ts
@@ -1,8 +1,29 @@
-import { useDispatch } from 'react-redux'
-import { StoreType } from 'store'
+import streamlit as st
 
-let store: StoreType
+# --- THAY THẾ CHO REDUX DISPATCH ---
 
-export type AppDispatch = typeof store.dispatch
+def update_user_state(user_data):
+    """
+    Tương đương với dispatch(loginSuccess(user_data))
+    """
+    st.session_state['user_info'] = user_data
+    # Streamlit sẽ tự động cập nhật lại giao diện sau khi state thay đổi
+    st.rerun()
 
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+def logout_dispatch():
+    """
+    Tương đương với dispatch(logout())
+    """
+    if 'user_info' in st.session_state:
+        del st.session_state['user_info']
+    st.rerun()
+
+# --- CÁCH SỬ DỤNG TRONG GIAO DIỆN ---
+
+def login_form():
+    st.subheader("Đăng nhập")
+    username = st.text_input("Username")
+    if st.button("Login"):
+        # Thay vì dispatch action, ta gọi hàm xử lý state trực tiếp
+        user_payload = {"name": username, "is_logged_in": True}
+        update_user_state(user_payload)
